df515a041466e2246668b83d7e40006785c034a2
Bogdan <bogdansavc@gmail.com>
2025-02-18 16:58:08 +0200

    refactoring

--- main.py
+++ main.py
@@ -19,6 +19,43 @@ def contains_in_list(element, list):
             return True
     return False
 
+def autorization(driver):
+    driver.get("https://www.favbet.ua/uk/login")
+    time.sleep(5)
+
+    email_field = driver.find_element("id", "email")
+    email_field.send_keys(os.getenv("EMAIL"))
+
+    password_field = driver.find_element("id", "password")
+    password_field.send_keys(os.getenv("PASSWORD"))
+
+    enter_button = driver.find_element("css selector", "form> button[data-role='login-page-submit-btn']")
+    enter_button.click()
+
+def parse_bets(driver):
+    bets_ids = []
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    selector_for_bets = "div.Bet_container--lCz:contains('Невизначено') span[data-role='bets-history-text-copy-id-text']"
+    parse_bets = soup.select(selector_for_bets)
+
+    for bet in parse_bets:
+        bet_id = re.search("\d+", bet.get_text())
+        bets_ids.append(bet_id.group(0))
+
+    return bets_ids
+
+def check_bets(bets_ids, previous_bets_ids):
+    if not previous_bets_ids and bets_ids:
+        previous_bets_ids.extend(bets_ids)
+        driver.save_screenshot(path)
+        asyncio.run(send_photo(path))
+    else:
+        for bet_id in bets_ids:
+            if not contains_in_list(bet_id, previous_bets_ids):
+                previous_bets_ids.append(bet_id)
+                driver.save_screenshot(path)
+                asyncio.run(send_photo(path))
+
 options = webdriver.ChromeOptions()
 options.add_argument("--disable-blink-features=AutomationControlled")
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
@@ -26,7 +63,6 @@ options.add_experimental_option("useAutomationExtension", False)
 
 bot = Bot(token=os.getenv("BOT_TOKEN"))
 
-#Авторизация
 driver = webdriver.Chrome(options=options)
 
 driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
@@ -41,58 +77,23 @@ driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
     '''
 })
 
-driver.get("https://www.favbet.ua/uk/login")
-time.sleep(5)
-
-email_field = driver.find_element("id", "email")
-email_field.send_keys(os.getenv("EMAIL"))
-
-password_field = driver.find_element("id", "password")
-password_field.send_keys(os.getenv("PASSWORD"))
-
-enter_button = driver.find_element("css selector", "form> button[data-role='login-page-submit-btn']")
-enter_button.click()
-print(enter_button.text)
+#Авторизация
+autorization(driver)
 
 time.sleep(5)
 
 driver.get("https://www.favbet.ua/uk/personal-office/bets/sport/")
-driver.fullscreen_window()
 
 #Начало цикла
 
-time.sleep(5)
+time.sleep(15)
 
-soup = BeautifulSoup(driver.page_source, 'html.parser')
-selector_for_bets = "div.Bet_container--lCz:contains('Невизначено') span[data-role='bets-history-text-copy-id-text']"
-parse_bets = soup.select(selector_for_bets)
-
-bets_ids = []
+bets_ids = parse_bets(driver)
 previous_bets_ids = []
 path = "./screenshots/file.png"
 
-for bet in parse_bets:
-    bet_id = re.search("\d+", bet.get_text())
-    bets_ids.append(bet_id.group(0))
-
-if not previous_bets_ids and bets_ids:
-    previous_bets_ids = bets_ids.copy()
-    driver.save_screenshot(path)
-
-    asyncio.run(send_photo(path))
-
-    print("copied")
-else:
-    for bet_id in bets_ids:
-        if not contains_in_list(bet_id, previous_bets_ids):
-            print("not contains!" + bet_id)
-            previous_bets_ids.append(bet_id)
-            driver.save_screenshot(path)
-
-            asyncio.run(send_photo(path))
-
-for bet_id in previous_bets_ids:
-    print(bet_id)
+#Поиск новых ставок
+check_bets(bets_ids, previous_bets_ids)
 
 driver.refresh()
 
